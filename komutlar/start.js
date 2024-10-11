9ba0d4c0091601758297eef7b9290d071e718406
Tilki <121335160+tilkidevelopment@users.noreply.github.com>
2024-10-12 01:33:00 +0300

    Create start.js

---
+++ komutlar/start.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'start',
+    description: 'Botun tanıtım komutu',
+    execute: (ctx) => {
+        const mesaj = `Merhaba ben tilki ^w^
+Komutlarıma bakmak için /yardim yaz!`;
+        const resimLinki = 'https://tilki.neocities.org/resim/tilki_afis.png'; 
+        ctx.replyWithPhoto(
+            { url: resimLinki },
+            {
+                caption: mesaj,
+            }
+        );
+    }
+};
